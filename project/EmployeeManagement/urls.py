e4d54678c3bf1d3997d62318942d8e582bd6c409
Hitesh <hiteshingole51@gmail.com>
2025-06-06 12:34:51 +0530

    changes

--- project/EmployeeManagement/urls.py
+++ project/EmployeeManagement/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from . import views 
+from . import views
 
-urlpatterns=[
-    path('home',views.homepage),
+urlpatterns = [
+    path('home', views.homepage),
     path('ecom',views.ecom),
     path('delete/<int:id>',views.deleteOrder),
-    path("alter/<int:id>",views.alterOrder),
+    path('alter/<int:id>',views.alterOrder),
     path('data',views.dataChange),
+    path('buy/<int:id>',views.buyProduct),
+    path('addToCart/<int:id>',views.addTocart),
+    path('removefromcart/<int:id>',views.removeFromcart),
+    path('cart',views.cartpage)
 ]
--- project/EmployeeManagement/views.py
+++ project/EmployeeManagement/views.py
@@ -1,48 +1,75 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import requests
-from .import models
-# Create your views here.
+import json
+from . import models
 
+# Create your views here.
+userID = 1
 
 def homepage(req):
-    carttable = models.cart.objects.all().values()
-    return render(req, 'index.html', context={'Orders': carttable})
-
+    cartTable = models.cart.objects.all().values()
+    return render(req,'index.html',context={'table':cartTable})
 
 def dataChange(req):
-    carttable = models.cart.objects.all().values()
-    return render(req, 'data.html', context={'Orders': carttable})
-
+    cartTable = models.cart.objects.all().values()
+    return render(req,'data.html',context={'table':cartTable})
+    
 def ecom(req):
-    response = requests.get('https://dummyjson.com/products', params={"limit": 20})
+    response = requests.get('https://dummyjson.com/products',params={"limit":20})
     data = response.json()
     print(data)
-    brandList = []
+    #   brandList = []
     # for i in data['products']:
-    #     if i['price'] == 9.99:
-    #         priceList.append(i)
-    # print(priceList)
-    return render(req, 'ecom.html', context={"productList": data['products'], 'brandproductList': brandList})
+    #     if i['brand'] == 'Essence':
+    #         brandList.append(i)
+    return render(req,'ecom.html',context={"productList":data['products']})
 
-
-def deleteOrder(req, id):
+def deleteOrder(req,id):
     order = models.cart.objects.get(id=id)
     order.delete()
-    # order.name = req.POST.get('name')
-    # order.size = req.POST.get('name')
-    # order.brand = req.POST.get('name')
-    # order.contact = req.POST.get('name')
-    # order.price = req.POST.get('name')
-    # order.save()
     return redirect('/home')
 
 def alterOrder(req,id):
-    if req.method=="POST":
-        order=models.cart.objects.get(id=id)
-        order.name=req.POST['productName']
+    if req.method == "POST":
+        order = models.cart.objects.get(id=id)
+        order.name = req.POST['productName']
         order.save()
         return redirect('/data')
     else:
         print(req.method)
-        order=models.cart.objects.get(id=id)
-        return render(req,'alter.html',context={"data":order})
+        order = models.cart.objects.get(id=id)
+        return render(req,"alter.html",context={"data":order})
+    
+def buyProduct(req,id):
+    response = requests.get(f'https://dummyjson.com/products/{id}')
+    data = response.json()
+    print(data)
+    return render(req,"buy.html",context={"data":data})
+
+def addTocart(req,id):
+   user = models.users.objects.get(id = userID)
+   usercart = json.loads(user.cart)
+   usercart.append(id)
+   user.cart = usercart
+   user.save()  
+   print(usercart)
+   return redirect(f"/buy/{id}?addedtocart=true")
+
+def removeFromcart(req,id):
+   user = models.users.objects.get(id = userID)
+   usercart = json.loads(user.cart)
+   usercart.remove(id)
+   user.cart = usercart
+   user.save()  
+   print(usercart)
+   return redirect(f"/cart?removefromcart=true")
+
+def cartpage(req):
+   user = models.users.objects.get(id = userID)
+   usercart = json.loads(user.cart)
+   cartItem= []
+   for i in usercart:
+        response = requests.get(f'https://dummyjson.com/products/{i}')
+        data = response.json()
+        cartItem.append(data)
+   return render(req,'cart.html',context={"cartitem":cartItem})
